3855779bd49199966fe755c60eb2a355dfcb940d
beniutek <bbeniutek@gmail.com>
2023-06-14 14:43:45 +0200

    drawer imporvements - languages wip

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,37 +1,62 @@
 // Drawer.tsx
 import { useState } from 'react';
+import Link from 'next/link';
 import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import List from '@mui/material/List';
+import InfoIcon from '@mui/icons-material/Info';
 import { styled } from '@mui/system';
 
 const StyledDrawer = styled(Drawer)(({ theme }) => ({
   width: '250px',
+  display: 'flex',
+  flexDirection: 'column',
 }));
 
 const DrawerComponent = ({open, toggleDrawer}) => {
+  const items = [
+    { name: 'O nas', type: 'top', icon: <InfoIcon /> },
+    { name: 'English', type: 'bottom', icon: <img src="/images/flags/gb.svg" alt="English" width={24} height={24} /> },
+    { name: 'Polski', type: 'bottom', icon: <img src="/images/flags/pl.svg" alt="Polski" width={24} height={24} /> },
+    { name: 'Ukrainian', type: 'bottom', icon: <img src="/images/flags/ua.svg" alt="Ukrainian" width={24} height={24} /> },
+  ];
+
   return (
-      <StyledDrawer
-        anchor="right"
-        open={open}
-        onClose={toggleDrawer(false)}
+    <StyledDrawer
+      anchor="right"
+      open={open}
+      onClose={toggleDrawer(false)}
+    >
+      <Box
+        sx={{ width: 250, height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
       >
-        <Box
-          sx={{ width: 250 }}
-          role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
-        >
-          <List>
-            {/* Pass your drawer list items here */}
-            <ListItem button>
-              <ListItemText primary={'Drawer Item'} />
-            </ListItem>
-          </List>
-        </Box>
-      </StyledDrawer>
+        <List sx={{ marginTop: 8 }}>
+          <ListItem component={Link} href="/about_us">
+            <ListItemText primary="O nas" />
+          </ListItem>
+        </List>
+        <List>
+          <ListItem>
+            <ListItemIcon><img src="/images/flags/gb.svg" alt="English" width={24} height={24} /></ListItemIcon>
+            <ListItemText primary="English" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon><img src="/images/flags/pl.svg" alt="Polski" width={24} height={24} /></ListItemIcon>
+            <ListItemText primary="Polski" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon><img src="/images/flags/ua.svg" alt="Український" width={24} height={24} /></ListItemIcon>
+            <ListItemText primary="Український" />
+          </ListItem>
+        </List>
+      </Box>
+    </StyledDrawer>
   );
 }
 
